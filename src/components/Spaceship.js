e8c86625d2df80529f7babd424b72adba417cbe6
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-29 19:20:47 +0000

    automatically backed up by learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 export default class Spaceship extends React.Component {
   render() {
     return (
-      <div className="spaceship">
+      <div>
         <h2>{this.props.name}</h2>
         <p>{this.props.speed}</p>
         <p>{this.props.hasRockets}</p>
